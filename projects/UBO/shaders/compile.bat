66792eba7da1702ee04458b252f9a82a9c9da2dc
Dillon Kyle <dillonkyle95@protonmail.com>
2025-09-17 10:24:18 -0700

    Removed project files

--- projects/UBO/shaders/compile.bat
+++
@@ -1,6 +0,0 @@
-glslc.exe water.vert -o compiledShaders/waterVert.spv
-glslc.exe water.frag -o compiledShaders/waterFrag.spv
-glslc.exe lit.vert -o compiledShaders/litVert.spv
-glslc.exe lit.frag -o compiledShaders/litFrag.spv
-
-pause
